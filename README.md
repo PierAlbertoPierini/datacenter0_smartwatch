e38a9c2737365d017faf859080f14de39cc8d275
Pier Alberto Pierini <pieralbertopierini@gmail.com>
2021-01-04 03:18:32 +0000

    Added a readme file and the license

---
+++ README.md
@@ -0,0 +1,48 @@
+#DataCenter 0 - The Smart Watch
+
+##The Idea
+This project started to build my own smart watch, why? Because I am tired to have objects builded for the mass, build with out thinking on reusability and always builded on closed source.
+I was thinking to have something modular that can grow with my needs and can be usable forever.
+One night I woke up and I was not able to sleep, my Idea was completely changing amplitude and WHY don't build a smart watch Open source that can be shaped from any one and be helpful at work and in any moment of a life of a person?
+
+##Where to use
+###DataCenter
+If you working in a DataCenter you can enjoy this smart watch because you can build it for your Level 1 or Level 2 Technicians, for each one you can have different visualizations:
+1. Monitoring the DataCenter: number of Servers Down
+2. Give in real time tasks at each Technician and have in real time the situation of the task
+3. Level 1 can visualize machines Down and Level 2 visualize Networks failures
+4. Visualize tasks in order of job to do
+5. Monitoring in Real Time critical temperatures in a Data Center 
+6. etc, the opportunities are endlless
+You can build it for every level of your organization and enjoy it.
+###Development
+It can help to monitoring in real time the code and deployment of the software version.
+Monitor the pull or push of your GitHub project.
+Monitor the pipe line of your project.
+###Home
+Monitoring your IoT appliances or your home servers or only show you the time and weather of the day. ;)
+
+##Why enjoy it
+Because it will be made from Open source and it will be entirely reusable and recyclable (no waste).
+Use a Python 3 that can speed up the development of your own application in your own environment, don't waste time and money to build something that don't fit with you.
+
+##Hardware
+###Raspberry Pi Zero W
+This smart watch is made around the Raspberry Pi Zero W, because is small, need less power and there are a lot Hats or modules that can be attached and used in many projects.
+###Display
+You can chose your own screen, from a LCD to a e-paper.
+###Power Supply
+A power supply already built for it or you can build your own.
+###Sensors
+Add all kind of sensors, by example in a DataCenter you can add a vibration motor to help workers to know the arrive of a news because there environment is noisy but for a Programmer you can switch it with an audio module.
+At home you can enjoy with the add of a microfone to be able to give vocal command to your smart watch.
+
+##Software
+###Raspberry OS
+Use of Linux distro Raspberry OS 32 bit that is Open source.
+In the same time you can have a portable Desktop Computer on your wrist.
+###Python 3
+With Python 3 you can enjoy the simplicity to programming your own personalized monitoring smart watch.
+
+
+Made with LOVE by a Global Citizen
--- main.py
+++ main.py
@@ -12,24 +12,24 @@ try:
     epd.Clear(0xFF)
 
 # Drawing on the image
-#    image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
-#    print("Drawing")
-#    draw = ImageDraw.Draw(image)
-#    draw.rectangle([(0,0),(50,50)],outline = 0)
-#    draw.rectangle([(55,0),(100,50)],fill = 0)
-#    draw.line([(0,0),(50,50)], fill = 0,width = 1)
-#    draw.line([(0,50),(50,0)], fill = 0,width = 1)
-#    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
-#    draw.ellipse((55, 60, 95, 100), outline = 0)
-#    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
-#    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
-#    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
-#    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
-#    font15 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif-Bold.ttf', 15)
-#    draw.text((110, 60), 'e-Paper demo', font = font15, fill = 0)
-#    draw.text((110, 80), 'Ciao Pier Alberto', font = font15, fill = 0)
-#    epd.display(epd.getbuffer(image))
-#    time.sleep(2)
+    image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
+    print("Drawing")
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([(0,0),(50,50)],outline = 0)
+    draw.rectangle([(55,0),(100,50)],fill = 0)
+    draw.line([(0,0),(50,50)], fill = 0,width = 1)
+    draw.line([(0,50),(50,0)], fill = 0,width = 1)
+    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
+    draw.ellipse((55, 60, 95, 100), outline = 0)
+    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
+    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
+    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
+    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
+    font15 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif-Bold.ttf', 15)
+    draw.text((110, 60), 'poppo\'', font = font15, fill = 0)
+    draw.text((110, 80), 'Cosmo', font = font15, fill = 0)
+    epd.display(epd.getbuffer(image))
+    time.sleep(2)
 
 # read bmp file
 #    print("read bmp file")
@@ -48,18 +48,18 @@ try:
 #    time.sleep(2)
 
 # # partial update
-    print("Show time")
-    epd.init(epd.PART_UPDATE)
-    epd.Clear(0xFF)
-    font24 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif-Bold.ttf', 24)
-    time_image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)
-    time_draw = ImageDraw.Draw(time_image)
-    while (True):
-        time_draw.rectangle((10, 10, 120, 50), fill = 255)
-        time_draw.text((10, 10), time.strftime('%H:%M'), font = font24, fill = 0)
-        newimage = time_image.crop([10, 10, 120, 50])
-        time_image.paste(newimage, (10,10))
-        epd.displayPartial(epd.getbuffer(time_image))
+#    print("Show time")
+#    epd.init(epd.PART_UPDATE)
+#    epd.Clear(0xFF)
+#    font24 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif-Bold.ttf', 24)
+#    time_image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)
+#    time_draw = ImageDraw.Draw(time_image)
+#    while (True):
+#        time_draw.rectangle((10, 10, 120, 50), fill = 255)
+#        time_draw.text((10, 10), time.strftime('%H:%M'), font = font24, fill = 0)
+#        newimage = time_image.crop([10, 10, 120, 50])
+#        time_image.paste(newimage, (10,10))
+#        epd.displayPartial(epd.getbuffer(time_image))
 
     epd.sleep()
 
